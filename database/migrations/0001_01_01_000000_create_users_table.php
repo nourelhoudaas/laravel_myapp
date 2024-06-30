1d3400f1540740599bfa97c4969672506158eb0c
nourelhoudaas <sayahhouda06@gmail.com>
2024-06-30 14:44:04 +0100

    update database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->string('id_user')->primary();
             $table->string('name');
-            $table->integer('id_nin')->unique()->foreign('id_nin')->references('id_nin')->on('employes');
-            $table->integer('id_p')->unique()->foreign('id_p')->references('id_p')->on('employes');
+            $table->integer('id_nin');
+            $table->foreign('id_nin')->references('id_nin')->on('employes')->unique();
+            $table->integer('id_p');
+            $table->foreign('id_p')->references('id_p')->on('employes')->unique();
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
