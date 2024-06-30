ec2dc7637cbafc58203fcd856920943f04c55a39
nourelhoudaas <sayahhouda06@gmail.com>
2024-06-30 23:29:45 +0100

    update

--- database/migrations/2024_06_30_104016_create_logins_table.php
+++ database/migrations/2024_06_30_104016_create_logins_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('logins', function (Blueprint $table) {
             $table->string('id_log')->primary();
             $table->date('date_login');
-            $table->date('date_logout')-nullable();
+            $table->date('date_logout')->nullable();
             $table->integer('id_nin')->unique();
             $table->integer('id_p')->unique();
             $table->foreign('id_nin')->references('id_nin')->on('Employe');
