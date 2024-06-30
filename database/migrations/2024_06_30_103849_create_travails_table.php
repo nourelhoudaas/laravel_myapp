fc4ce6f533bca4806813bddf71ae63d19c754dc1
nourelhoudaas <sayahhouda06@gmail.com>
2024-06-30 16:43:07 +0100

    update database

--- database/migrations/2024_06_30_103849_create_travails_table.php
+++ database/migrations/2024_06_30_103849_create_travails_table.php
@@ -19,9 +19,9 @@ return new class extends Migration
             $table->integer('id_nin')->unique();
             $table->integer('id_p')->unique();
             $table->string('id_bureau')->unique();
-            $table->integer('id_nin')->references('id_nin')->on('Employe');
-            $table->integer('id_p')->references('id_p')->on('Employe');
-            $table->integer('id_bureau')->references('id_bureau')->on('Bureau');
+            $table->foreign('id_nin')->references('id_nin')->on('Employe');
+            $table->foreign('id_p')->references('id_p')->on('Employe');
+            $table->foreign('id_bureau')->references('id_bureau')->on('Bureau');
         });
     }
 
