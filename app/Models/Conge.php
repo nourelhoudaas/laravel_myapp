4aba26f6169bde956f9c4900035ef320372fc5a2
Sid-AB <ayoub.06.08.10@gmail.com>
2024-07-17 02:04:11 +0100

    update

--- app/Models/Conge.php
+++ app/Models/Conge.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Conge extends Model
 {
     use HasFactory;
+    protected $table = 'conges';
+    protected $primaryKey = 'id_cong';
+    public $incrementing = true; 
+    protected $keyType = 'integer'; 
+      // Désactiver les timestamps automatiques
+      public $timestamps = false;
+
+      protected $fillable = ['id_cong','date_debut_cong',	'date_fin_cong'	,'total_jour',	'ref_cong','id_nin','id_p'];
 }
