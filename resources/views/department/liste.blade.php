016342c1d2b5b74b624219c57e9b082214fbf60c
nourelhoudaas <sayahhouda06@gmail.com>
2024-08-08 10:59:18 +0100

    df

--- resources/views/department/liste.blade.php
+++ resources/views/department/liste.blade.php
@@ -4,65 +4,64 @@
 @section('title', 'liste Directions')
 
 @section('content')
+
     <body>
 
-        <!-- start section aside -->
-        @include('./navbar.sidebar')
-        <!-- end section aside -->
-        <h1 class="app-page-title">{{ __('lang.title_list_direc') }}  <a href="#" class="btn btn-primary">Ajouter une direction</a></h1>
- <br></br>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>{{ __('lang.id_drec') }} </th>
-                    <th>{{ __('lang.nom_direct') }} </th>
-                    <th>{{ __('lang.nom_sous_direct') }} </th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($departements as $departement )
-                    <tr>
-                        <td>{{ $departement->id_depart }}</td>
-                        <td>{{$departement->Nom_depart }}</td>
-                        <td>{{$departement->Nom_sous_depart }}</td>
+        <div class="container2">
 
-                        <td>
-                            <style>
-                                .fa-edit {
+            <!-- start section aside -->
+            @include('./navbar.sidebar')
+            <!-- end section aside -->
+            <main>
+                <div class="recent_order">
+                    <h1 class="app-page-title">{{ __('lang.title_list_direc') }}
+                        <a href="#"class="btn btn-primary">Ajouter une direction</a></h1>
+                    <br></br>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>{{ __('lang.id_drec') }} </th>
+                                <th>{{ __('lang.nom_direct') }} </th>
+                                <th>{{ __('lang.nom_sous_direct') }} </th>
+                                <th>{{ __('lang.Action') }}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($departements as $departement)
+                                <tr>
+                                    <td>{{ $departement->id_depart }}</td>
+                                    <td>{{ $departement->Nom_depart }}</td>
+                                    <td>{{ $departement->Nom_sous_depart }}</td>
 
-                                    font-size:30px;
-                                }
-                            </style>
-                            <a href="{{route('departement.editer',$departement->id_depart)}}"><i class="fa fa-edit" ></i></a>
+                                    <td>
 
-                            <form action="#" method="POST" style="display:inline;">
-                                @csrf
-                                @method('DELETE')
-                                <style>
-                                    .fa-trash {
-                                        color: #e40b0b;
-                                        font-size:30px;
-                                    }
-                                </style>
-                                <a onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet direction ?')" href="{{route('department.delete',$departement->id_depart)}}"> <i class="fa fa-trash" aria-hidden="true"></i></a>
-                            </form>
+                                        <a href="{{ route('departement.editer', $departement->id_depart) }}"><i
+                                                class="fa fa-edit"></i></a>
+
+                                        <form action="#" method="POST" style="display:inline;">
+                                            @csrf
+                                            @method('DELETE')
+
+                                            <a onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet direction ?')"
+                                                href="{{ route('department.delete', $departement->id_depart) }}"> <i
+                                                    class="fa fa-trash" aria-hidden="true"></i></a>
+                                        </form>
 
 
 
-                        </td>
-
-                @endforeach
-            </tbody>
-        </table>
-        <nav class="app-pagination">
-            {{$departements->links()}}
+                                    </td>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <nav class="app-pagination">
+                        {{ $departements->links() }}
 
 
 
 
-		</nav><!--//app-pagination-->
+                    </nav><!--//app-pagination-->
 
 
-
-        @endsection
+                </div>
+                </main>
+                @endsection
