fe749f647be676684d17b2e4e3fc029d3de93319
nourelhoudaas <sayahhouda06@gmail.com>
2024-09-05 11:48:02 +0100

    add

--- resources/lang/fr/lang.php
+++ resources/lang/fr/lang.php
@@ -220,7 +220,7 @@ return [
 
 
     'encadrement'=> 'Encadrements',
-    'maîtrise'=>'Maîtrise',
+    'maîtrise'=>'Maîtrises',
     'executif'=>'Executifs',
     'nbr_emp_depart'=>'Répartition des employés pour chaque département',
     'nbr_emp'=>'Nombre d\'employer'
