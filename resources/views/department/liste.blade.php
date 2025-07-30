a49265d1f25bad727d6263aa7c73767552db80c4
nourelhoudaas <sayahhouda06@gmail.com>
2025-07-30 15:15:44 +0100

    add

--- resources/views/department/liste.blade.php
+++ resources/views/department/liste.blade.php
@@ -13,127 +13,69 @@
                 <h1>{{ __('lang.msg_list_direct_ssdirect') }}</h1>
             </div>
 
-            <table class="styled-table" id='myTable_depart'>
-                <thead>
-                    <tr>
-<<<<<<< HEAD
-                        <th>{{ __('lang.id_drec') }} </th>
-                        <th>{{ __('lang.nom_direct') }} </th>
-                        <th>{{ __('lang.nom_sous_direct') }} </th>
-                        {{-- <th>{{ __('lang.Action') }}</th> --}}
-                    </tr>
-                </thead>
-                <tbody>
-                    @php
-                    $locale = app()->getLocale();
-                    @endphp
-=======
-                                <th>{{ __('lang.id_drec') }} </th>
-                                <th>{{ __('lang.nom_direct') }} </th>
-                                <th>{{ __('lang.nom_sous_direct') }} </th>
-                                 <th>{{ __('lang.Action') }}</th> 
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php
-                             $locale = app()->getLocale();
-                             @endphp
->>>>>>> 35bd43192fb4db4bced46617af2bb2fef10cb233
+<table class="styled-table" id='myTable_depart'>
+    <thead>
+        <tr>
+            <th>{{ __('lang.id_drec') }} </th>
+            <th>{{ __('lang.nom_direct') }} </th>
+            <th>{{ __('lang.nom_sous_direct') }} </th>
+            <th>{{ __('lang.Action') }}</th> 
+        </tr>
+    </thead>
+    <tbody>
+        @php
+        $locale = app()->getLocale();
+        @endphp
 
-                    @foreach ($departements as $index => $departement)
-                    <tr>
-                        <td>{{ $index + 1 }}</td>
-                        <td>
-
-                            @if ($locale == 'fr')
-                            <a
-                                href="{{ route('app_dashboard_depart', $departement->id_depart) }}">{{ $departement->Nom_depart }}</a>
-
-<<<<<<< HEAD
-                            @elseif ($locale == 'ar')
-                            <a
-                                href="{{ route('app_dashboard_depart', $departement->id_depart) }}">{{ $departement->Nom_depart_ar }}</a>
-                            @endif
-                        </td>
-                        <td>
-                            @if($departement->sous_departement->isNotEmpty())
-                            @foreach ($departement->sous_departement as $sous_departement)
-                            @if ($locale == 'fr')
-                            <a href="/listcontient{{$sous_departement->Nom_sous_depart}}">
-                                {{ $sous_departement->Nom_sous_depart }}<br>
-                            </a>
-                            @elseif ($locale == 'ar')
-                            <a href="/listcontient/{{$sous_departement->Nom_sous_depart_ar}}">
-                                {{ $sous_departement->Nom_sous_depart_ar }}<br>
-                            </a>
-                            @endif
-=======
-                                            @elseif ($locale == 'ar')
-                                            <a
-                                            href="{{ route('app_dashboard_depart', $departement->id_depart) }}">{{ $departement->Nom_depart_ar }}</a>
-                                            @endif
-                                    </td>
-                                    <td>
-
-                                        @foreach ($departement->sous_departement as $sous_departement)
-                                        @if ($locale == 'fr')
-                                        <a
-                                        href="/listcontient{{$sous_departement->Nom_sous_depart}}"> {{ $sous_departement->Nom_sous_depart }}<br></a>
-
-
-                                            @elseif ($locale == 'ar')
-                                            <a
-                                            href="/listcontient/{{$sous_departement->Nom_sous_depart_ar}}"> {{ $sous_departement->Nom_sous_depart_ar }}<br>
-                                            @endif
-                                        @endforeach
-                                    </td>
-                                    
-                                    <td>
-                                             <a href="{{ route('departement.editer', $departement->id_depart) }}"><i
-                                                class="fa fa-edit"></i></a>
-                                    </td>
-                                    {{-- <td>
-
-                                        <a href="{{ route('departement.editer', $departement->id_depart) }}"><i
-                                                class="fa fa-edit"></i></a>
-
-                                         <form action="#" method="POST" style="display:inline;">
-                                            @csrf
-                                            @method('DELETE')
-
-                                            <a onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet direction ?')"
-                                                href="{{ route('department.delete', $departement->id_depart) }}"> <i
-                                                    class="fa fa-trash" aria-hidden="true"></i></a>
-                                        </form>
-
-
-
-                                    </td> --}}
->>>>>>> 35bd43192fb4db4bced46617af2bb2fef10cb233
-                            @endforeach
-                            @else
-                            <span class="text-muted">Aucun sous-département</span>
-                            @endif
-                        </td>
-
-                        {{-- <td>
-                                        <a href="{{ route('departement.editer', $departement->id_depart) }}"><i
-                            class="fa fa-edit"></i></a>
-
-                        <form action="#" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-
-                            <a onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet direction ?')"
-                                href="{{ route('department.delete', $departement->id_depart) }}"> <i class="fa fa-trash"
-                                    aria-hidden="true"></i></a>
-                        </form>
-                        </td> --}}
-                    </tr>
-
+        @foreach ($departements as $index => $departement)
+        <tr>
+            <td>{{ $index + 1 }}</td>
+            <td>
+                @if ($locale == 'fr')
+                <a href="{{ route('app_dashboard_depart', $departement->id_depart) }}">
+                    {{ $departement->Nom_depart }}
+                </a>
+                @elseif ($locale == 'ar')
+                <a href="{{ route('app_dashboard_depart', $departement->id_depart) }}">
+                    {{ $departement->Nom_depart_ar }}
+                </a>
+                @endif
+            </td>
+            <td>
+                @if($departement->sous_departement->isNotEmpty())
+                    @foreach ($departement->sous_departement as $sous_departement)
+                        @if ($locale == 'fr')
+                        <a href="/listcontient{{ $sous_departement->Nom_sous_depart }}">
+                            {{ $sous_departement->Nom_sous_depart }}<br>
+                        </a>
+                        @elseif ($locale == 'ar')
+                        <a href="/listcontient/{{ $sous_departement->Nom_sous_depart_ar }}">
+                            {{ $sous_departement->Nom_sous_depart_ar }}<br>
+                        </a>
+                        @endif
                     @endforeach
-                </tbody>
-            </table>
+                @else
+                    <span class="text-muted">Aucun sous-département</span>
+                @endif
+            </td>
+            <td>
+                <a href="{{ route('departement.editer', $departement->id_depart) }}">
+                    <i class="fa fa-edit"></i>
+                </a>
+
+                <form action="{{ route('department.delete', $departement->id_depart) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <a onclick="return confirm('Êtes-vous sûr de vouloir supprimer cette direction ?')" href="#">
+                        <i class="fa fa-trash" aria-hidden="true"></i>
+                    </a>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
             
 
 
